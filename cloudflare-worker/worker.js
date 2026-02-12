eed8f9c02c89bbca22125efe36c040bbd8970bf9
Mac <ptbosk@gmail.com>
2026-02-12 06:27:19 -0500

    change worker perms

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
     const corsHeaders = {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type'
     };
 
